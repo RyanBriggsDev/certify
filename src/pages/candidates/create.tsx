e9dc59e31972436a9ed180ae8dd45133ca2dacae
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-04-04 15:25:26 +0100

    added company option to create candidate

--- src/pages/candidates/create.tsx
+++ src/pages/candidates/create.tsx
@@ -7,25 +7,91 @@ import { candidateOnClient } from '@/lib/schema'
 import { ZodError } from 'zod'
 import { AlertContext } from '@/lib/AlertContext'
 import { useRouter } from 'next/router'
+import { getCompanies } from '../api/company'
 
-export default function CreateCandidate() {
+type CreateCandidateProps = {
+  companies: object
+}
+
+export default function CreateCandidate(props: CreateCandidateProps) {
+  let companies: string[] = []
+  function prepareCompanyArr(companyData) {
+    if (companyData.length > 0) {
+      for (let i = 0; i < companyData.length; i++) {
+        companies.push(companyData[i].name + ' - ID: ' + companyData[i].id)
+      }
+    }
+  }
+  prepareCompanyArr(props.companies)
+
   return (
     <Frame>
       <H1>Create Candidate</H1>
       <div className="flex w-full justify-around gap-2">
-        <CreateCandidateForm />
+        <CreateCandidateForm companies={companies} />
       </div>
     </Frame>
   )
 }
 
 // Had to break Form out into function as context is not available until 'Frame' component is rendered
-function CreateCandidateForm() {
+function CreateCandidateForm(props) {
   const { setAlert } = useContext(AlertContext) as any
   const [loading, setLoading] = useState(false)
   const router = useRouter()
 
-  async function handleSubmit(form: unknown) {
+  const formContent = [
+    {
+      title: 'Create new candidate',
+      desc: 'Fill out the form below to make a new candidate.',
+      inputs: [
+        {
+          label: 'Name',
+          type: 'text',
+          name: 'name',
+          placeholder: 'John Doe',
+          required: true,
+        },
+        {
+          label: 'Email',
+          type: 'email',
+          name: 'email',
+          placeholder: 'johndoe@gmail.com',
+          required: true,
+        },
+        {
+          label: 'Address',
+          type: 'text',
+          name: 'address',
+          placeholder: '12 Placeholder road, Placeholder, Place',
+        },
+        {
+          label: 'Telephone Number',
+          type: 'text',
+          name: 'telephoneNumber',
+          placeholder: '02830434565',
+        },
+        {
+          label: 'Company',
+          type: 'select',
+          name: 'company',
+          placeholder: 'Please select an option',
+          options: props.companies,
+        },
+      ],
+      button: {
+        text: 'Create',
+        type: 'primary',
+      },
+    },
+  ]
+
+  async function handleSubmit(form: any) {
+    if (!form.name || !form.company) {
+      return setAlert('Please fill out required form fields. ')
+    }
+    const { company, ...candidateData } = form
+    candidateData.companyId = form.company.split(' ').slice(-1).toString()
     setLoading(true)
     try {
       const checked = await candidateOnClient.parse(form)
@@ -64,40 +130,11 @@ function CreateCandidateForm() {
   )
 }
 
-const formContent = [
-  {
-    title: 'Create new candidate',
-    desc: 'Fill out the form below to make a new course.',
-    inputs: [
-      {
-        label: 'Name',
-        type: 'text',
-        name: 'name',
-        placeholder: 'John Doe',
-        required: true,
-      },
-      {
-        label: 'Email',
-        type: 'email',
-        name: 'email',
-        placeholder: 'johndoe@gmail.com',
-        required: true,
-      },
-      {
-        label: 'Address',
-        type: 'text',
-        name: 'address',
-        placeholder: '12 Placeholder road, Placeholder, Place',
-      },
-      {
-        label: 'Telephone Number',
-        type: 'text',
-        name: 'telephoneNumber',
-      },
-    ],
-    button: {
-      text: 'Create',
-      type: 'primary',
-    },
-  },
-]
+export async function getServerSideProps(context) {
+  const companies = await getCompanies(context)
+  const stringed = JSON.stringify(companies)
+  const parsed = JSON.parse(stringed)
+  return {
+    props: { companies: parsed },
+  }
+}
